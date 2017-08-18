8fcd6e9b338599f688d9a596e28f9691bbbeeeb7
Marianna Almeida <agyrafa@gmail.com>
2017-08-18 13:02:04 -0300

    remove loader

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,8 @@
 $(window).load(function () {
 
     // preloader
-    $('#status').fadeOut(); // will first fade out the loading animation
-    $('#preloader').delay(550).fadeOut('slow'); // will fade out the white DIV that covers the website.
+    // $('#status').fadeOut(); // will first fade out the loading animation
+    // $('#preloader').delay(550).fadeOut('slow'); // will fade out the white DIV that covers the website.
     $('body').delay(550).css({
         'overflow': 'visible'
     });
